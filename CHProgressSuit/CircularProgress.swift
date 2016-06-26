9b40e8b482a74344b20b8e0da452552cdadaf06e
Calvin <calvin.peak@capslock.tw>
2016-06-26 11:04:47 +0900

    Add cocoa pods.

---
+++ CHProgressSuit/CircularProgress.swift
@@ -0,0 +1,21 @@
+//
+//  CircularProgress.swift
+//  CHProgressSuit
+//
+//  Created by Calvin on 6/26/16.
+//  Copyright © 2016 CapsLock. All rights reserved.
+//
+
+import UIKit
+
+class CircularProgress: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
