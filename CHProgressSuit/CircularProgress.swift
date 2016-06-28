242161db95e5472e515633e8b65358679969385c
Calvin <calvin.peak@capslock.tw>
2016-06-28 16:42:25 +0900

    - Complete animation - Assign properties default value by private method

--- CHProgressSuit/CircularProgress.swift
+++ CHProgressSuit/CircularProgress.swift
@@ -7,24 +7,25 @@
 //
 
 import UIKit
+import CHCubicBezier
 
 @IBDesignable class CircularProgress: UIView {
-    @IBInspectable var progressBarColor: UIColor = UIColor(red: 33/255.0, green: 150/255.0, blue: 243/255.0, alpha: 1) {
+    @IBInspectable var progressBarColor: UIColor! {
         didSet {
             progressBar.strokeColor = progressBarColor.CGColor
         }
     }
-    @IBInspectable var progressRingColor: UIColor = UIColor(red: 187/255.0, green: 222/255.0, blue: 251/255.0, alpha: 1) {
+    @IBInspectable var progressRingColor: UIColor! {
         didSet {
             progressRing.strokeColor = progressRingColor.CGColor
         }
     }
-    @IBInspectable var fillColor: UIColor = UIColor.clearColor() {
+    @IBInspectable var fillColor: UIColor! {
         didSet {
             fillBackground.fillColor = fillColor.CGColor
         }
     }
-    @IBInspectable var textColor: UIColor = UIColor(red: 47/255.0, green: 125/255.0, blue: 183/255.0, alpha: 1) {
+    @IBInspectable var textColor: UIColor! {
         didSet {
             progressText.foregroundColor = textColor.CGColor
         }
@@ -39,7 +40,20 @@ import UIKit
             }
             
             progressBar.strokeEnd = currentProgress
-            progressText.string = "\((Int)(currentProgress * 100))"
+            progress = currentProgress
+            
+            countingStartTime = NSDate.timeIntervalSinceReferenceDate()
+            countingStartValue = oldValue
+            
+            if displayLink == nil {
+                displayLink = CADisplayLink(target: self, selector: #selector(updateProgressText))
+                displayLink?.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+            }
+        }
+    }
+    @IBInspectable var duration: Double = 0.25 {
+        didSet {
+//            progressBar.duration = duration
         }
     }
     
@@ -48,6 +62,12 @@ import UIKit
     private let fillBackground = CAShapeLayer()
     private let progressText = CATextLayer()
     private let fontWeightMedium: CGFloat = 0.230000004172325
+    
+    private let cubicBezier = CubicBezier(mX1: 0.42, mY1: 0, mX2: 0.58, mY2: 1)
+    
+    private var displayLink: CADisplayLink?
+    private var countingStartTime: NSTimeInterval!
+    private var countingStartValue: CGFloat = 0
     
     // Default style is designed in width 168
     private var scaleRate: CGFloat {
@@ -66,17 +86,20 @@ import UIKit
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        self.initPropertiesDefaultValue()
         self.initUI()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
+        self.initPropertiesDefaultValue()
         self.initUI()
     }
     
     // MARK: - Override Methods
     override func layoutSubviews() {
+        
         super.layoutSubviews()
         
         let rectInCenter = CGRect(origin: CGPoint(x: progressWidth / 2, y: progressWidth / 2), size: CGSize(width: progressLength, height: progressLength))
@@ -102,17 +125,41 @@ import UIKit
         progressText.frame = CGRect(origin: CGPoint(x: 0, y: self.bounds.height / 2 - textSize.height / 2), size: CGSize(width: self.bounds.width, height: textSize.height))
     }
     
+    // MARK: - Selectors
+    func updateProgressText() {
+        let nowTime = NSDate.timeIntervalSinceReferenceDate()
+        let changeTime = nowTime - countingStartTime
+        
+        if changeTime > Double(duration) {
+            displayLink?.invalidate()
+            displayLink = nil
+        } else {
+            let changeValue = progress - countingStartValue
+            var t = changeTime / duration
+            
+            if t > 0.91 {
+                t = 1
+            }
+            
+            progressText.string = "\((Int)((countingStartValue + changeValue * (CGFloat)(cubicBezier.easing(t))) * 100))"
+        }
+    }
+    
     // MARK: - Private Methods
+    private func initPropertiesDefaultValue() {
+        progressBarColor = UIColor(red: 33/255.0, green: 150/255.0, blue: 243/255.0, alpha: 1)
+        progressRingColor = UIColor(red: 187/255.0, green: 222/255.0, blue: 251/255.0, alpha: 1)
+        fillColor = UIColor.clearColor()
+        textColor = UIColor(red: 47/255.0, green: 125/255.0, blue: 183/255.0, alpha: 1)
+        progress = 0
+    }
+    
     private func initUI() {
-        // Assign Default values manully
-        progressBar.strokeColor = progressBarColor.CGColor
-        progressRing.strokeColor = progressRingColor.CGColor
-        progressText.foregroundColor = textColor.CGColor
         progressBar.fillColor = UIColor.clearColor().CGColor
         progressRing.fillColor = UIColor.clearColor().CGColor
-        fillBackground.fillColor = fillColor.CGColor
-        progressBar.strokeEnd = progress
-        progressText.string = "\((Int)(progress * 100))"
+        
+        // Disable implict animation
+        progressText.actions = ["contents": NSNull()]
         
         progressText.contentsScale = UIScreen.mainScreen().scale
         progressText.alignmentMode = kCAAlignmentCenter
